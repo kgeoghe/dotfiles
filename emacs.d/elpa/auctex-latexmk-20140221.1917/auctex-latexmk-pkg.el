c0e9b6a199c6e02c27930d76a82bf726f7b3bd58
Kevin Geoghegan <kgeoghe@gmail.com>
2015-01-31 22:08:28 -0500

    Major .emacs overhaul

--- emacs.d/elpa/auctex-latexmk-20140221.1917/auctex-latexmk-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "auctex-latexmk" "20140221.1917" "Add LatexMk support to AUCTeX" (quote ((auctex "11.87"))))
--- emacs.d/elpa/workgroups2-20140225.1510/workgroups2-pkg.el
+++
@@ -1,2 +0,0 @@
-(define-package "workgroups2" "20140225.1510" "New workspaces for Emacs"
-  '((cl-lib "0.4")))
