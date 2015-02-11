e02650e411f5c482a827be3dbf818e600fe7cb50
kevin geoghegan <kgeoghe@Kevins-MacBook-Pro.local>
2015-02-11 12:50:39 -0500

    Allowed org-mode tables to use fixed-width font so tables align properly

--- emacs.d/settings/custom.el
+++ emacs.d/settings/custom.el
@@ -3,26 +3,30 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
-)
+ )
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- (if (window-system)
-     '(magit-item-highlight ((t (:inherit highlight :background "#042028"))))
-   '(magit-item-highlight ((t (:background "white" :foreground "black")))))
- (if (window-system)
-     '(mode-line ((t (:background "#0a2832" :foreground "#eee8d4" :inverse-video t :box nil :underline nil :slant normal :weight normal)))))
  '(ein:cell-input-area ((t (:background "#042028"))))
  '(ein:cell-input-prompt ((t (:inherit header-line :background "#002b35" :foreground "#859900" :inverse-video nil :weight bold))))
  '(ein:cell-output-prompt ((t (:inherit header-line :background "#002b35" :foreground "#dc322f" :inverse-video nil :weight bold))))
+ '(font-latex-bold-face ((((class color) (background light)) (:bold t))))
+ '(font-latex-italic-face ((((class color) (background light)) (:italic t))))
+ '(font-latex-math-face ((((class color) (background light)) (:foreground "blue4"))))
+ '(font-latex-sedate-face ((((class color) (background light)) (:foreground "Gray"))))
+ '(font-latex-string-face ((t (:foreground "red4"))))
  '(font-lock-comment-face ((t (:foreground "#6171c4" :inverse-video nil :underline nil :slant italic :weight normal))))
+ '(font-lock-constant-face ((((class color) (background light)) (:foreground "     Blue"))))
+ '(font-lock-doc-string-face ((((class color) (background light)) (:foreground "green4"))))
  '(font-lock-function-name-face ((t (:foreground "#2075c7" :inverse-video nil :underline nil :slant normal :weight bold))))
  '(font-lock-keyword-face ((t (:foreground "#cb4b16" :inverse-video nil :underline nil :slant normal :weight normal))))
+ '(font-lock-reference-face ((((class color) (background light)) (:foreground "red4"))))
  '(font-lock-type-face ((t (:foreground "#d33682" :inverse-video nil :underline nil :slant normal :weight normal))))
  '(fringe ((t (:background "#002b35" :foreground "#465a61"))))
+ '(magit-item-highlight ((t (:inherit highlight :background "#042028"))))
  '(markdown-header-face-1 ((t (:inherit markdown-header-face :height 210))))
  '(markdown-header-face-2 ((t (:inherit markdown-header-face :height 190))))
  '(markdown-header-face-3 ((t (:inherit markdown-header-face :height 170))))
@@ -30,5 +34,8 @@
  '(markdown-header-face-5 ((t (:inherit markdown-header-face :slant italic :weight bold))))
  '(markdown-header-face-6 ((t (:inherit markdown-header-face :slant italic :weight normal))))
  '(markdown-math-face ((t (:inherit font-lock-string-face :foreground "#cb4b16" :slant italic))))
+ '(mode-line ((t (:background "#0a2832" :foreground "#eee8d4" :inverse-video t :box nil :underline nil :slant normal :weight normal))))
  '(mumamo-background-chunk-major ((t (:background "#002b36"))))
  '(py-variable-name-face ((t (:inherit default :foreground "#268bd2")))))
+
+
