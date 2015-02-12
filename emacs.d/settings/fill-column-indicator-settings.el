e6831ebdea785321a0743bdbe65f72f70fed5a63
Kevin Geoghegan <kgeoghe@gmail.com>
2015-02-11 20:14:56 -0500

    Adjusted fci-mode so it's only active for coding--not while I'm trying to write.

--- emacs.d/settings/fill-column-indicator-settings.el
+++ emacs.d/settings/fill-column-indicator-settings.el
@@ -4,8 +4,13 @@
 
 (require 'fill-column-indicator)
 
-(define-globalized-minor-mode
- global-fci-mode fci-mode (lambda () (fci-mode 1)))
-(global-fci-mode t)
+; this section would enable fci-mode for all buffers as a minor mode
+;;(define-globalized-minor-mode
+;; global-fci-mode fci-mode (lambda () (fci-mode 1)))
+;;(global-fci-mode t)
+
+;alternatively, I like to enable fci-mode for specific file types so I can keep writing buffers
+;(e.g., TeX) fci free
+(add-hook 'prog-mode-hook 'fci-mode)
 
 (provide 'fill-column-indicator-settings)
