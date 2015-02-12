582115f82826f50e8e68bba22e1c9e15f5b783c0
Kevin Geoghegan <kgeoghe@gmail.com>
2015-02-12 18:14:57 -0500

    Added lisp for modified summary buffer--nice!

--- emacs.d/settings/gnus-settings.el
+++ emacs.d/settings/gnus-settings.el
@@ -35,8 +35,7 @@
 (setq gnus-use-cache t)
 
 ;; BBDB: Address list
-(add-to-list 'load-path "~/")
-(require 'bbdb)
+(require 'bbdb-loaddefs "~/.emacs.d/el-get/bbdb/lisp/bbdb-loaddefs.el")
 (bbdb-initialize 'message 'gnus 'sendmail)
 (setq bbdb-file "~/.bbdb") ;; OPTIONAL, because I'm sharing my ~/.emacs.d
 (add-hook 'gnus-startup-hook 'bbdb-insinuate-gnus)
@@ -83,5 +82,47 @@
       smtpmail-local-domain "homepc")
 ;; http://www.gnu.org/software/emacs/manual/html_node/gnus/_005b9_002e2_005d.html
 (setq gnus-use-correct-string-widths nil)
+
+; borrowed from TomRauchenwald at http://emacswiki.org/emacs/TomRauchenwald
+;; eye candy
+(copy-face 'font-lock-variable-name-face 'gnus-face-6)
+(setq gnus-face-6 'gnus-face-6)
+(copy-face 'font-lock-constant-face 'gnus-face-7)
+(setq gnus-face-7 'gnus-face-7)
+(copy-face 'gnus-face-7 'gnus-summary-normal-unread)
+(copy-face 'font-lock-constant-face 'gnus-face-8)
+(set-face-foreground 'gnus-face-8 "gray50")
+(setq gnus-face-8 'gnus-face-8)
+(copy-face 'font-lock-constant-face 'gnus-face-9)
+(set-face-foreground 'gnus-face-9 "gray70")
+(setq gnus-face-9 'gnus-face-9)
+(setq gnus-summary-make-false-root 'dummy)
+(setq gnus-summary-make-false-root-always nil)
+(defun oxy-unicode-threads () 
+  (interactive)
+  (setq gnus-summary-dummy-line-format "    %8{│%}   %(%8{│%}                       %7{│%}%) %6{□%}  %S\n"
+	gnus-summary-line-format "%8{%4k│%}%9{%U%R%z%}%8{│%}%*%(%-23,23f%)%7{│%} %6{%B%} %s\n"
+	gnus-sum-thread-tree-indent " "
+	gnus-sum-thread-tree-root "■ "
+	gnus-sum-thread-tree-false-root "□ "
+	gnus-sum-thread-tree-single-indent "▣ "
+	gnus-sum-thread-tree-leaf-with-other "├─▶ "
+	gnus-sum-thread-tree-vertical "│"
+	gnus-sum-thread-tree-single-leaf "└─▶ "))
+
+(defun oxy-unicode-threads-heavy () 
+  (interactive)
+  (setq gnus-summary-line-format "%8{%4k│%}%&user-date;%8{│%}%9{%U%R%z%}%8{│%}%*%(%-23,23f%)%7{║%} %6{%B%} %s\n"
+	gnus-summary-dummy-line-format "    %8{│%}                %8{│%}   %(%8{│%}                       %7{║%}%) %6{┏○%}  %S\n"
+        gnus-user-date-format-alist '((t . "%Y-%m-%d %H:%M"))
+	gnus-sum-thread-tree-indent " "
+	gnus-sum-thread-tree-root "┏● " 
+	gnus-sum-thread-tree-false-root " ○ "
+	gnus-sum-thread-tree-single-indent " ● "
+	gnus-sum-thread-tree-leaf-with-other "┣━━❯ " 
+	gnus-sum-thread-tree-vertical "┃"
+	gnus-sum-thread-tree-single-leaf "┗━━❯ "))
+
+(oxy-unicode-threads-heavy)
 
 (provide 'gnus-settings)
