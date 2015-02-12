cab94ef88b36cf2cf7bd9adc57aef73b8dfb8e0b
Kevin Geoghegan <kgeoghe@gmail.com>
2015-02-11 20:15:54 -0500

    Updated

--- emacs.d/settings/org-settings.el
+++ emacs.d/settings/org-settings.el
@@ -4,7 +4,7 @@
 
 (setq org-use-fast-todo-selection t)
 (setq org-use-tag-inheritance nil)
-;(setq org-startup-indented t)
+(setq org-startup-indented t)
 (define-key global-map "\C-cl" 'org-store-link)
 
 (provide 'org-settings)
