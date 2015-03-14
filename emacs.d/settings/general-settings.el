88f42bb009a938d41367b306fb8849750fb32848
Kevin Geoghegan <kgeoghe@gmail.com>
2015-03-14 12:10:53 -0400

    Comment out line that adds /usr/local/bin to env path a second time. Pretty sure this is redundant.

--- emacs.d/settings/general-settings.el
+++ emacs.d/settings/general-settings.el
@@ -5,7 +5,7 @@
 ; set PATH, because we don't load .bashrc
 ; function from https://gist.github.com/jakemcc/3887459
 (defun set-exec-path-from-shell-PATH ()
-  (setenv "PATH" (concat "/usr/local/bin:" (getenv "PATH")))
+  ;(setenv "PATH" (concat "/usr/local/bin:" (getenv "PATH"))) ; don't think this is necessary
   (let ((path-from-shell (shell-command-to-string "$SHELL -i -c 'echo -n $PATH'")))
     (setenv "PATH" path-from-shell)
     (setq exec-path (split-string path-from-shell path-separator))))
