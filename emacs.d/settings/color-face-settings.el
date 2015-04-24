3b85c430cbd6491583eef49facf3217b0699b658
Kevin Geoghegan <kgeoghe@gmail.com>
2015-04-24 10:11:08 -0400

    Fixed clock time in mode line so it's not so noticeable

--- emacs.d/settings/color-face-settings.el
+++ emacs.d/settings/color-face-settings.el
@@ -25,14 +25,24 @@
 			:foundry "apple" 
 			:family "DejaVu_Sans_Mono"))
 
+;;;_ , Org Mode
 ; allows proper alignment of Org Mode tables
 (set-face-attribute 'org-table nil :foreground "Dark Gray"  :inherit 'fixed-pitch)
 (set-face-attribute 'org-date nil :foreground "#d33682" :inherit 'fixed-pitch)
 (set-face-attribute 'org-link nil :foreground "#d33682" :underline t :inherit 'fixed-pitch)
 
+; default behavior is to have the org clock time part of the mode line 'highlighted' in every
+; window--I like the whole mode line to have the same active/inactive color face settings
+(set-face-attribute 'org-mode-line-clock nil :inherit nil)
+
 ; don't use the hideous default red in level 4 org headings
 (set-face-attribute 'org-level-4 nil :foreground "#859900")
+; TODO better 'TODO' colors--might be nice, but currently my solarized theme overrides
+;; (set-face-attribute 'org-todo nil
+;;                     :weight 'bold :box '(:line-width 1 :color "#D8ABA7")
+;;                     :foreground "#D8ABA7" :background "#FFE6E4")
 
+;;;_ , Other Modes
 ;; Use variable width font faces in current buffer
  (defun my-buffer-face-mode-variable ()
    "Set font to a variable width (proportional) fonts in current buffer"
